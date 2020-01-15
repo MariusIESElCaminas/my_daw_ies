962acfff4be741276dd49985959c6eec2a4628b1
marius <alux9575076s@ieselcaminas.org>
2020-01-15 09:19:44 +0100

    One more

--- febrer.php
+++ febrer.php
@@ -2,3 +2,4 @@ echo 'pel febrer, carnestoles el primer';
 echo 'Pel febrer flor a l'ametler';
 echo 'Més tard o més matiner carnaval dins el febrer';
 echo 'Febrer, Febrearet, set capes i un barret';
+echo 'Has de llaurar al Febrer el que al marc vulgues sembrar';
